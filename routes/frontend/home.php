0be45616937524ce2f94ee7f038d57ad0a79d3a7
Uzair-412 <99256003+Uzair-412@users.noreply.github.com>
2022-03-15 18:45:37 +0500

    Add files via upload

---
+++ routes/frontend/home.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\loginController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/sign-up',  [loginController::class, 'sign_up'])->name('signup');
